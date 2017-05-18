caeeb9ea1e817ee714823c89c2092adcf3f3126f
WRBTS\spallagani <spallagani@wrberkley.com>
2017-05-18 17:25:41 -0500

    JPA + RDS setup.

--- src/main/java/com/suresh/projects/movieworld/controllers/MovieController.java
+++ src/main/java/com/suresh/projects/movieworld/controllers/MovieController.java
@@ -1,13 +1,8 @@
 package com.suresh.projects.movieworld.controllers;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.joda.time.DateTime;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.suresh.projects.movieworld.entities.Movie;
@@ -22,31 +17,22 @@ public class MovieController {
 	@GetMapping("/movies")
 	public String findMovies() {
 		try {
-			List<Movie> movies = new ArrayList();
 			Movie movie = new Movie();
 			movie.setRatingId(1);
-			movie.setTitle("Test Movie");
+			movie.setTitle("Test Movie 2");
 			movie.setYearReleased(2017);
 			movie.setDateReleased(DateTime.now().toDate());
-			movies.add(movie);
-			Iterable<Movie> movies1 = movieRepository.findAll();
-//			return String.valueOf(movie.getId());
-//			String url = "jdbc:mysql://myrdsmysql.c5n3azxbdqmp.us-east-2.rds.amazonaws.com:3306/";
-//			String userName = "myrdsmysql";
-//			String password = "myrdsmysql";
-//			String dbName = "myrdsmysql";
-//			String driver = "com.mysql.jdbc.Driver";
-//			Class.forName(driver);
-//			Connection connection = DriverManager.getConnection(url + dbName, userName, password);	
-			return "Test Success";
+			movieRepository.save(movie);
+			Iterable<Movie> movies = movieRepository.findAll();
+			return String.valueOf(movie.getId() + " " +movie.getTitle());
 		} catch (Exception e) {
 			e.printStackTrace();
 			return "Test Failed";
 		}
 	}
 	
-	@PostMapping("/movies")
-	public String createMovies(@RequestBody List<Movie> movies) {
-		return "Created";
-	}
+//	@PostMapping("/movies")
+//	public String createMovies(@RequestBody List<Movie> movies) {
+//		return "Created";
+//	}
 }
--- src/main/java/com/suresh/projects/movieworld/entities/Movie.java
+++ src/main/java/com/suresh/projects/movieworld/entities/Movie.java
@@ -8,10 +8,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.Table;
 
 @Entity
-@Table(name = "movie")
 public class Movie implements Serializable{
 	
 	@Id
