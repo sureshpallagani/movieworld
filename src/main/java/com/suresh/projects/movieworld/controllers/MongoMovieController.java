c7e18bf7adcb39094ddb3bba56fbf6fce56e35e1
THINKPAD-X230\Owner <sureshpallagani@gmail.com>
2017-07-10 22:36:13 -0500

    MongoDB - Part 3 QueryDSL

--- src/main/java/com/suresh/projects/movieworld/controllers/MongoMovieController.java
+++ src/main/java/com/suresh/projects/movieworld/controllers/MongoMovieController.java
@@ -2,6 +2,7 @@ package com.suresh.projects.movieworld.controllers;
 
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
+import static org.springframework.util.StringUtils.isEmpty;
 
 import javax.validation.constraints.NotNull;
 
@@ -24,6 +25,7 @@ import com.suresh.projects.movieworld.entities.Movie;
 import com.suresh.projects.movieworld.repositories.mongo.MongoMovieRepository;
 
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 
 @RestController
 public class MongoMovieController {
@@ -31,13 +33,24 @@ public class MongoMovieController {
 	@Autowired
 	private MongoMovieRepository repository;
 	
-	@ApiOperation(value = "Movies Search Options")
+	@ApiOperation(value = "Search Movies", notes = "End-point for various search options on movies")
 	@GetMapping(value = "/mongo/movies", produces = MediaType.APPLICATION_JSON_VALUE)
 	public PagedResources<Resource<Movie>> findAll(Pageable pageable, PagedResourcesAssembler<Movie> assembler, 
-													@RequestParam(name = "actors", required = false) String actors,
-													@RequestParam(name = "directors", required = false) String directors,
-													@RequestParam(name = "genres", required = false) String genres) throws Exception {
-		
+													@ApiParam(value = "Can be one or more actors in the form of actor1|actor2|...", required = false) 
+														@RequestParam(name = "actors", required = false) String actors,
+													@ApiParam(value = "Can be one or more directors in the form of director1|director2|...", required = false) 
+														@RequestParam(name = "directors", required = false) String directors,
+													@ApiParam(value = "Can be one or more genres in the form of genre1|genre2|...", required = false) 
+														@RequestParam(name = "genres", required = false) String genres,
+													@ApiParam(value = "Find movies by year", required = false)
+														@RequestParam(name="year", required = false) int year) throws Exception {
+		if (year > 0) {
+			return assembler.toResource(repository.findByYear(year, pageable));
+		}
+		if (!isEmpty(actors)) {
+			QMovie movie = new QMovie();
+			return assembler.toResource(repository.findAll(pageable));
+		}
 		return assembler.toResource(repository.findAll(pageable));
 	}
 	
@@ -48,12 +61,13 @@ public class MongoMovieController {
 		return new Resource<Movie>(repository.findOne(id), selfLink);
 	}
 	
-	@ApiOperation(value = "Create movie")
+	@ApiOperation(value = "Create a movie")
 	@PostMapping(value = "/mongo/movies", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
 	public Movie createMovie(@RequestBody @NotNull Movie movie) throws Exception {
 		return repository.save(movie);
 	}
 
+	@ApiOperation(value = "Delete a movie")
 	@DeleteMapping("/mongo/movies/{id}")
 	public void deleteMovie(@PathVariable Long id) {
 		repository.delete(id);
--- src/main/java/com/suresh/projects/movieworld/repositories/mongo/MongoMovieRepository.java
+++ src/main/java/com/suresh/projects/movieworld/repositories/mongo/MongoMovieRepository.java
@@ -1,11 +1,16 @@
 package com.suresh.projects.movieworld.repositories.mongo;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.querydsl.QueryDslPredicateExecutor;
 import org.springframework.stereotype.Repository;
 
 import com.suresh.projects.movieworld.entities.Movie;
 
 @Repository
-public interface MongoMovieRepository extends MongoRepository<Movie, Long> {
+public interface MongoMovieRepository extends MongoRepository<Movie, Long>, QueryDslPredicateExecutor<Movie> {
+
+	Page<Movie> findByYear(int year, Pageable pageable);
 
 }
